ef65135830821ef5e4f379c261c80b32b98c38b8
TinkGu <tlwrite@126.com>
2017-12-03 22:12:17 +0800

    [update] change modules export

--- src/index.js
+++ src/index.js
@@ -1,32 +1,116 @@
-import { create } from 'rxjs/observable/create'
-import { of } from 'rxjs/observable/of'
+import Rx from 'rxjs/Rx'
 
-const ob = {}
+export const withGetApp = wrappedPage =>
+    Object.assign(wrappedPage, {
+        getApp,
+    })
 
-// eslint-disable-next-line
-for (const p in wx) {
-    switch (typeof wx[p]) {
-        case 'object':
-            ob[p] = Object.assign(wx[p])
-            break
-        case 'function':
-            if (/Sync$/.test(p)) {
-                ob[p] = obj => of(wx[p].call(null, obj))
-            } else {
-                ob[p] = obj =>
-                    create(observer => {
-                        const param = Object.assign({}, obj)
-                        param.success = (...arg) => observer.next(...arg)
-                        param.fail = e => observer.error(e)
-                        param.complete = () => observer.complete()
-                        wx[p].call(null, param)
-                    })
-            }
-            break
-        default:
-            ob[p] = wx[p]
-            break
+export function connect(selector) {
+    return (wrappedPage) => {
+        const page = Object.assign({}, wrappedPage)
+        let connectSub
+
+        page.onLoad = function onLoad(...params) {
+            const app = getApp()
+            const didUpdate$ = app.store$
+                .map(x => selector(x))
+                .distinctUntilChanged((x, y) =>
+                    Object.keys(x).every(k => x[k] === y[k]))
+            connectSub = didUpdate$.subscribe(x =>
+                this.state$ ? this.state$.next(x) : this.setData(x))
+            apply(this, wrappedPage, 'onLoad', params)
+        }
+
+        page.onUnload = function onUnload(...params) {
+            tryUnsubcribe(connectSub)
+            apply(this, wrappedPage, 'onUnload', params)
+        }
+        return page
     }
 }
 
-export default ob
+const dump = () => {}
+const highOrderDump = () => dump
+export function watchUpdate(wrappedPage) {
+    const page = Object.assign({}, wrappedPage)
+    page.state$ = new Rx.Subject()
+
+    page.onLoad = function onLoad(...params) {
+        if (!this.state$ || this.state$.closed) {
+            this.state$ = new Rx.Subject()
+        }
+        const didUpdate = this.didUpdate
+            ? oldVal => () => this.didUpdate(oldVal)
+            : highOrderDump
+
+        this.state$
+            .map(x => this.willUpdate
+                ? (this.willUpdate(Object.assign({}, this.data, x)) || x)
+                : x)
+            .subscribe(x => {
+                const data = Object.assign({}, this.data)
+                this.setData(x, didUpdate(data))
+            })
+        apply(this, wrappedPage, 'onLoad', params)
+    }
+
+    page.onUnload = function onUnload(...params) {
+        tryUnsubcribe(this.state$)
+        apply(this, wrappedPage, 'onUnload', params)
+    }
+
+    return page
+}
+
+// 加入 computed 属性，必须前置 watchUpdate
+export function computable(wrappedPage) {
+    const page = Object.assign({}, wrappedPage)
+    page.data = Object.assign(page.data, compute(page))
+    page.willUpdate = function willUpdate(nextState) {
+        const next = Object.assign({}, nextState, compute(this))
+        return apply(this, wrappedPage, 'willUpdate', [next])
+    }
+    return page
+}
+
+export function compose(funcs) {
+    if (funcs.length === 0) {
+        return arg => arg
+    }
+
+    if (funcs.length === 1) {
+        return funcs[0]
+    }
+
+    return funcs.reduce((a, b) => (...args) => a(b(...args)))
+}
+
+function tryUnsubcribe(subscription) {
+    if (subscription) {
+        subscription.unsubcribe()
+    }
+}
+
+function apply(ctx, target, funcName, args) {
+    if (target[funcName]) {
+        target[funcName].apply(ctx, args)
+    }
+}
+
+function compute(page) {
+    return page.computed
+        ? Object.keys(page.computed).reduce((x, k) => ({
+            ...x,
+            [k]: page.computed[k].apply(page),
+        }), {})
+        : {}
+}
+
+export const polyfill = compose([
+    withGetApp,
+])
+
+export const observable = compose([
+    computable,
+    watchUpdate,
+])
--- src/store.js
+++ src/store.js
@@ -1,19 +1,14 @@
-import { BehaviorSubject } from 'rxjs/BehaviorSubject'
-import { merge } from 'rxjs/observable/merge'
-// eslint-disable-next-line no-unused-vars
-import { map } from 'rxjs/operator/map'
-// eslint-disable-next-line no-unused-vars
-import { do as tap } from 'rxjs/operator/do'
+import Rx from 'rxjs/Rx'
 
 export function createStore(reducer$, options = {}) {
     const { debug = false } = options
     let state = {}
-    const store$ = new BehaviorSubject(state)
+    const store$ = new Rx.BehaviorSubject(state)
 
     const subscription = reducer$
         .map(reducer => reducer(state))
         // eslint-disable-next-line no-console
-        .tap(x => debug && console.log('%c%s', 'color:#61bd4f', '[wx-enhancer-store]: ', x))
+        .do(x => debug && console.log('%c%s', 'color:#61bd4f', '[wx-enhancer-store]: ', x))
         .subscribe(x => {
             state = x
             store$.next(x)
@@ -35,7 +30,7 @@ export function combineReducers(reducerMap) {
         ...state,
         [k]: r(state[k])
     })))
-    return merge(...reducers)
+    return Rx.Observable.merge(...reducers)
 }
 
 export function init(o$) {
